b9f2812e9643066c3e2533c9346fcbc94eb4ef3e
marcin <grzegorzewski@flyingdog.de>
2024-04-23 23:15:07 +0200

    fix

--- __init__.py
+++ __init__.py
@@ -17,11 +17,3 @@ DEFAULT_USER = "guest"
 # NODE_CLASS_MAPPINGS = {}
 __all__ = ['NODE_CLASS_MAPPINGS']
 version = "V1.0.0"
-
-print(f"### Loading: Gyre ({version})")
-workspace_path = os.path.join(os.path.dirname(__file__))
-comfy_path = os.path.dirname(folder_paths.__file__)
-db_dir_path = os.path.join(workspace_path, "db")
-
-workspace_app = web.Application()
-
