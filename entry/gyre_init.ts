7f24140577ddd79a49a2fd8804dab18b62e56323
imperator <mausz@flyingdog.de>
2024-04-25 00:25:03 +0200

    some renaming

---
+++ entry/gyre_init.ts
@@ -0,0 +1,8 @@
+console.log("fds-gradient-editor load now")
+//import { gradientEditor } from "./node_modules/@fds-components/fds-gradient-editor/dist/fds-gradient-editor-es.mjs";
+
+
+   var script = document.createElement("script");
+script.async = false;
+script.src ="/extensions/gyre-extensions/node_modules/@fds-components/fds-gradient-editor/dist/fds-gradient-editor.ts";
+document.head.appendChild(script);
