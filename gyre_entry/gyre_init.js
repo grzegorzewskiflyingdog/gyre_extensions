67fed002eb7b7421acc36d71bdcf150ddc0cc644
imperator <mausz@flyingdog.de>
2024-04-26 14:52:55 +0200

    json fix

--- gyre_entry/gyre_init.js
+++ gyre_entry/gyre_init.js
@@ -1,4 +1,4 @@
 console.log("fds-gradient-editor load now")
 //import { gradientEditor } from "./node_modules/@fds-components/fds-gradient-editor/dist/fds-gradient-editor-es.mjs";
 
-globalThis.gyre.loadScript("/node_modules/@fds-components/fds-gradient-editor/dist/fds-gradient-editor.js")
+globalThis.gyre.loadScript("node_modules/@fds-components/fds-gradient-editor/dist/fds-gradient-editor.js")
