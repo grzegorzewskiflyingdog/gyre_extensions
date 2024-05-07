54d3d3a3bd8166365a4ef6ae23d583bf7589940f
imperator <mausz@flyingdog.de>
2024-05-08 00:48:14 +0200

    gradient slider

--- gyre_entry/gyre_init.js
+++ gyre_entry/gyre_init.js
@@ -1,2 +1,3 @@
-// load custom component 
+// load custom components 
 globalThis.gyre.loadScript("node_modules/@fds-components/fds-gradient-editor/dist/fds-gradient-editor.js")
+globalThis.gyre.loadScript("node_modules/@fds-components/fds-gradient-slider/dist/fds-gradient-slider.js")
